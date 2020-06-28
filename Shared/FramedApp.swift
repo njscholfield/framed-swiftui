ca177a3ac34d535c103c7497333aaaab0af279d6
Noah Scholfield <contact@noahscholfield.com>
2020-06-28 08:36:24 -0400

    Initial Commit

---
+++ Shared/FramedApp.swift
@@ -0,0 +1,17 @@
+//
+//  FramedApp.swift
+//  Shared
+//
+//  Created by Noah Scholfield on 6/28/20.
+//
+
+import SwiftUI
+
+@main
+struct FramedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
